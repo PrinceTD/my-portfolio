7ea68e43c3c18e1a31e8a9e8035a622465faa70c
princeTD <talukderprince100@gmail.com>
2021-11-30 14:58:40 +0600

    added

--- src/Componets/Resume/Resume.js
+++ src/Componets/Resume/Resume.js
@@ -1,9 +1,21 @@
+
 import React from 'react';
+import TableEdu from './Table/TableEdu';
 
 const Resume = () => {
     return (
-        <div>
-            <h3>Education</h3>
+        <div className='container'>
+            <h5><strong>Education</strong></h5>
+            <TableEdu></TableEdu>
+
+            <div>
+            <h5><strong>Language</strong></h5>
+            <span>
+                <p>
+                    Bangla and English
+                </p>
+            </span>
+            </div>
         </div>
     );
 };
---
+++ src/Componets/Resume/Table/TableEdu.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const TableEdu = () => {
+    return (
+        <div>
+            <table class="table table-bordered border-secondary">
+  <thead>
+    <tr>
+      <th scope="col">Degree</th>
+      <th scope="col">Board/University</th>
+      <th scope="col">Group/Subject</th>
+      <th scope="col">Year</th>
+      <th scope="col">Result</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Honors</th>
+      <td>National University</td>
+      <td>Physics</td>
+      <td>2019-20</td>
+      <td>--</td>
+    </tr>
+    <tr>
+      <th scope="row">HSC</th>
+      <td>Sylhet Board</td>
+      <td>Seience</td>
+      <td>2018-19</td>
+      <td>4.75</td>
+    </tr>
+    <tr>
+      <th scope="row">SSC</th>
+      <td>Sylhet Board</td>
+      <td>Seience</td>
+      <td>2016-17</td>
+      <td>5.00</td>
+    </tr>
+   
+  </tbody>
+</table>
+        </div>
+    );
+};
+
+export default TableEdu;
