3371fb7f5bd531c7fc031717948cdc38262f9c18
princeTD <talukderprince100@gmail.com>
2021-11-30 23:37:07 +0600

    added

--- src/Componets/Project/OneProject.js
+++ src/Componets/Project/OneProject.js
@@ -12,7 +12,7 @@ const OneProject = (props) => {
                 <img className='img-border' src={imgProfile} alt="" width="100%" />
                 <div className="card-body">
                     <p className="card-title"><strong>{name}</strong>.
-                        <Link to={`/project/:${_id}`}>
+                        <Link to={`/project/${_id}`}>
                             <span style={{ color: "green" }}><i className="fas fa-arrow-circle-right"></i></span>
                         </Link></p>
 
--- src/Componets/Project/SingleService/SingleService.js
+++ src/Componets/Project/SingleService/SingleService.js
@@ -1,6 +1,8 @@
-import { Link } from '@mui/material';
+
+import Button from '@mui/material/Button';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const SingleService = () => {
     const [service, setService] = useState({});
@@ -10,14 +12,44 @@ const SingleService = () => {
         fetch(`https://thawing-meadow-37880.herokuapp.com/myservice/${serviceId}`)
             .then(res => res.json())
             .then(data => setService(data));
+        console.log(service);
 
     }, []);
-    
+
     return (
         <div className='cotainer'>
-            <div >
-                {serviceId}
-               
+            <div className="text-center containe pt-2">
+                <h2>
+                    {service.name}
+                </h2>
+                <p>
+                    {service.details}
+                </p>
+
+
+                <div className='row p-2'>
+                    <div className='col-md-4'>
+                        <span> <img src={service.imgProfile} class="d-block" style={{ width: "100%" }}></img></span>
+
+                    </div>
+                    <div className='col-md-4'>
+                        <span> <img src={service.imgProfile} class="d-block" style={{ width: "100%" }}></img></span>
+                    </div>
+                    <div className='col-md-4'>
+                        <span> <img src={service.imgProfile} class="d-block " style={{ width: "100%" }}></img></span>
+                    </div>
+                    <a style={{ textDecoration: "none", color: "blue" }} href={service.liveLink} target="_blank">
+                        <Button style={{marginTop:"5px"}} variant="outlined" size="small">
+                            Live Website
+                        </Button>
+                    </a>
+                   <Link  style={{ textDecoration: "none", color: "blue" }} to="/project">
+                        <Button style={{marginTop:"5px", paddingLeft:"20px", paddingRight:"20px"}} variant="contained" size="small">
+                            Go back
+                        </Button>
+                  </Link>
+                </div>
+
 
             </div>
 
